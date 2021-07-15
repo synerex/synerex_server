6c54dbb5b93b09bbb6c1af23ec0372db59cad6dd
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2021-07-15 20:27:18 +0900

    update for syslog error for docker

--- synerex-server_linux.go
+++ synerex-server_linux.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log/syslog"
 	"time"
-
+	"log"
 	"github.com/rcrowley/go-metrics"
 )
 
 func InitMetricsLog() {
-	w, _ := syslog.Dial("unixgram", "/dev/log", syslog.LOG_INFO, "synerex_metrics")
+	w, err := syslog.Dial("unixgram", "/dev/log", syslog.LOG_INFO, "synerex_metrics")
+	if err != nil {
+		log.Print("Error with metric:", err)
+		return
+	}
 	go metrics.Syslog(metrics.DefaultRegistry, 60*time.Second, w)
 }
